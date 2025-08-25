df397033b66b0da12a0dfd8755a8abdfffdf30a2
Natalia01rg <natalia01rg@estudiantec.cr>
2025-08-24 22:04:01 -0600

    Se agrega la carpeta backend

---
+++ programa/backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// --- INICIO DEL SERVIDOR ---
+app.listen(PORT, () => {
+  console.log(`Servidor backend de Adivinar numeros corriendo en http://localhost:${PORT}`);
+});
